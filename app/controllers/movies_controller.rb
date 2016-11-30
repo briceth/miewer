ff2208a21323251352ada7ddd0a78dfe7d703f14
Brice Tessier Huort <tessierhuort@gmail.com>
2016-11-30 20:30:41 +0100

    implement the simple form for movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,29 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @movie = Movie.new
   end
 
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.user = current_user
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :index
+    end
+
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :date)
+  end
+
 end
