0049af98b1260ed2432e0ac8bb02e1ec1b302906
Brice Tessier Huort <tessierhuort@gmail.com>
2016-12-01 10:07:07 +0100

    implement polymorphic association between user movies for now

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,7 +3,7 @@ class Author < ApplicationRecord
   has_many :filmings
   has_many :movies, trough: :filmings
   has_many :actors, trough: :filmings
-  has_many :reviews, as: :review
+  has_many :reviews, as: :reviewable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
   belongs_to :user, optional: true
   has_many :filmings
   has_many :actors, through: :filmings
-  has_many :reviews, as: :review
+  has_many :reviews, as: :reviewable
 
   validates :title, presence: true, uniqueness: true
 end
