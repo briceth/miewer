784bc3e6b3fb2ef1d3f15607e3e85157589a8c6a
Brice Tessier Huort <tessierhuort@gmail.com>
2016-12-02 12:42:16 +0100

    add category string to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202110519) do
+ActiveRecord::Schema.define(version: 20161202114135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20161202110519) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.boolean  "available"
+    t.string   "category"
     t.index ["user_id"], name: "index_movies_on_user_id", using: :btree
   end
 
