0a0125ad8272d0e7b5996fe13d14d1a12a63a142
Brice Tessier Huort <tessierhuort@gmail.com>
2016-12-03 17:57:55 +0100

    start the partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
-  # root to: 'movies#index'
 
   resources :users, only: [:show, :edit, :update, :destroy]
 
@@ -11,16 +10,12 @@ Rails.application.routes.draw do
   end
 
 
-  resources :movies, only: [:index, :show, :create] do
+  resources :movies, only: [:index, :show, :create, :delete] do
     concerns :reviews, only: [:create]
     resources :actors, module: :movies
   end
-  resources :actors, only: [:index, :show] do
+  resources :actors, only: [:index, :show, :delete] do
     concerns :reviews, only: [:create]
-      # post :review, on: :member
-
-      # post '/proposals/search' => 'proposals#search'
-      # resources :movies
   end
 
   resources :authors, only: [:index, :show] do
